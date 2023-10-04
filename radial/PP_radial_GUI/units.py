4b8d05809f7761a176d0fde3c64887768f0809dd
jmspar <jmspar@ulb.ac.be>
2023-10-04 11:33:33 +0200

    Unit choice: atomic or nuclear + particle masses (in console -> to be improved later)

--- radial/PP_radial_GUI/units.py
+++ radial/PP_radial_GUI/units.py
@@ -4,30 +4,36 @@ if __name__ == "__main__":
 
 import scipy.constants as cst
 
-print('Select physical problem')
-print('1. Electron in spherical quantum dot')
-print('2. Nucleus-nucleus central interaction')
-n_pb = int(input('   please enter problem number: '))
+print('Select unit system')
+print('1. Atomic physics')
+print('2. Nuclear physics')
+n_pb = int(input('   please enter unit system number: '))
+
+m1 = float(input('   enter mass of particle 1: '))
+m2 = float(input('   enter mass of particle 2: '))
+mu = m1*m2/(m1+m2)
 
 if n_pb == 1:
-	print('Electron in quantum dot')
+	print('Atomic physics')
 
 	# units
 	print('Length unit = nm')
 	print('Energy unit = eV')
+	print('Mass unit = electron mass')
 
-	h2m = cst.hbar**2/2/cst.m_e/cst.e*1e18 # hbar**2/2 m_e (eV nm**2)
+	h2m = cst.hbar**2 / 2 / mu / cst.m_e / cst.e*1e18 # hbar**2/2 m_e (eV nm**2)
 
-	print('hbar**2/2m_e=', h2m, 'eV nm**2')
+	print('hbar**2/2mu=', h2m, 'eV nm**2')
 
 elif n_pb == 2:
-	print('Nucleus-nucleus central interaction')
+	print('Nuclear physics')
 
 	# units
 	print('Length unit = fm')
 	print('Energy unit = MeV')
+	print('Mass unit = atomic mass unit')
 
-	h2m = cst.hbar ** 2 / 2 / cst.m_p / cst.e * 1e24  # hbar**2/2 m_e (MeV fm**2)
+	h2m = cst.hbar ** 2 / 2 / mu / cst.atomic_mass / cst.e * 1e24  # hbar**2/2 m_e (MeV fm**2)
 
 	print('hbar**2/2mu=', h2m, 'MeV fm**2')
 
